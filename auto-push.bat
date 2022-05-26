1dbaf78aed9bf3a36a4da438e427fc7e7eb828fb
YinggangDong <believerD@aliyun.com>
2022-05-26 11:05:16 +0800

    feat:auto-push脚本

--- auto-push.bat
+++ auto-push.bat
@@ -1,3 +1,13 @@
 @echo off
-echo this is a auto-push bat
+echo this is a auto-push bat~
+set /p var=please enter the feature name which want push ~
 
+echo %var%
+
+git cehckout %var%
+
+git merge origin/master
+
+git push --set-upstream origin %var%
+
+git 
