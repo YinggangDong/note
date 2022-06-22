6c0ceb31e37fa939a7835e5b2f2b79e9f0415d1e
YinggangDong <believerD@aliyun.com>
2022-06-22 10:58:35 +0800

    feat:git bat

--- auto-branch.bat
+++ auto-branch.bat
@@ -1,17 +1,25 @@
 @echo off
-set /p var=please enter the feature name ~
-echo %var%
+set /p var=please enter the branch name ~
+echo [1] create new branch: %var%
 
+echo [2] checkout master
 git checkout master
 
-git pull
+echo [3] pull origin master
+git pull origin master
 
+echo [4] create new branch %var%
 git branch %var%
 
+echo [5] checkout %var%
 git checkout %var%
 
+echo [6] push %var% to origin
 git push --set-upstream origin %var%
 
+echo [7] checkout master
 git checkout master
+
+echo [8] all end
 
 pause
--- auto-push.bat
+++ auto-push.bat
@@ -1,31 +1,37 @@
 @echo off
 echo this is a auto-push bat~
-set /p var=please enter the feature name which want push ~
+set /p var=please enter the branch name which want push :
 
-echo %var%
+echo [1] the branch want push: %var%
 
-echo checkout master and pull from origin
+echo [2] checkout master 
 git checkout master
 
+echo [3] pull origin master
 git pull origin master
 
-echo checkout target feature and merge master
+echo [4] checkout target feature
 git checkout %var%
 
+echo [5] merge master
 git merge master
 
-echo push target feature to origin
+echo [6] push target feature to origin
 git push --set-upstream origin %var%
 
-echo checkout develop and pull from origin
+echo [7] checkout develop 
 git checkout develop 
 
+echo [8] pull origin develop
 git pull origin develop
 
-echo merge new code
+echo [9] merge target feature
 git merge %var%
 
-echo push develop
+echo [10] push develop to origin
 git push --set-upstream origin develop
+
+echo [11] all end
+echo all end
 
 pause
